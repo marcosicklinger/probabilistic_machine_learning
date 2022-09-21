3803b28417b7f86fba3bd9acae9764892ca69d47
Marco Sicklinger <sicklinger.marco@gmail.com>
2022-09-21 13:38:28 +0400

    parameters feeding improvement

--- exam_project/DGPQModel.py
+++ exam_project/DGPQModel.py
@@ -5,7 +5,7 @@ from Utils import *
 n_trackers = 1
 low = [0, 0]
 high = [1, 1]
-lr_pars = {'gamma': .9999, 'LQ': 9, 'eps_1': 0.001, 'delta_1': 0.1, 'exploration': 0.1, 'start_update': 1e7}
+lr_pars = {'gamma': .8, 'LQ': 9, 'epsilon': 5, 'exploration': 0.1, 'RBF_length_scale':0.05,  'start_update': 1e7}
 hyper_pars = {'time_importance': 1, 'max_time': 200}
 current = RandomCurrentVelocity(-3, 3)
 environment = DGPQEnvironment(n_trackers, low, high, lr_pars, hyper_pars, current=current, memoryless_trackers=True)
@@ -15,5 +15,5 @@ environment = DGPQEnvironment(n_trackers, low, high, lr_pars, hyper_pars, curren
 # print('\t- {} angles state aggregation'.format(2*environment.orientation_slices))
 # print('----------------------------------------------------------')
 
-n_episodes = 1001
-environment.train(n_episodes, render=[1000], remember=False)
+n_episodes = 5001
+environment.train(n_episodes, render=[1000, 4900, 4950, 5000], remember=False)
